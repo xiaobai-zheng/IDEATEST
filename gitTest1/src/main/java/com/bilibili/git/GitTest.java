371551628445dac703185cc5696a2c01bbe85f65
qiang_glb <888888_glb@zheng.com>
2019-12-08 21:56:28 +0800

    选择了后端

--- gitTest1/src/main/java/com/bilibili/git/GitTest.java
+++ gitTest1/src/main/java/com/bilibili/git/GitTest.java
@@ -3,7 +3,8 @@ package com.bilibili.git;
 public class GitTest {
     private String name;
     private String email;
-    private String string="我是前后端合并来的";
+    private String string="我是前后端合并来的 我选择了从后端出来的";
+
     public String getName() {
         return name;
     }
