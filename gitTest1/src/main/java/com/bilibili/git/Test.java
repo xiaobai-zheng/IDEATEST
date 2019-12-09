cc32631a4d969351ed1eaad1e600a7d16d2236f8
qiang_glb <888888_glb@zheng.com>
2019-12-09 12:25:20 +0800

    测试单个文件提交

--- gitTest1/src/main/java/com/bilibili/git/Test.java
+++ gitTest1/src/main/java/com/bilibili/git/Test.java
@@ -3,5 +3,6 @@ package com.bilibili.git;
 public class Test {
     public static void main(String[] args){
         System.out.println("可以成功运行");
+        System.out.println("我是测试单个文件的提交");
     }
 }
